df541858ae23daae731bfc19da4c61a65c1d0707
SIsilicon <sisilicon28@gmail.com>
2019-09-27 09:01:19 -0500

    remove mis-named folder

--- UFO-0.8.0/Effects/Explosion/Explosion.gd
+++
@@ -1,9 +0,0 @@
-extends AnimatedSprite
-
-func _ready():
-	$AnimationPlayer.play("explode")
-	play()
-
-func _physics_process(delta):
-	modulate.a *= 3
-
--- UFO-0.8.0/Effects/Pulse/Pulse.gd
+++
@@ -1,8 +0,0 @@
-extends AnimatedSprite
-
-func _ready():
-	play("default")
-
-func _physics_process(delta):
-	if self.frame == 31:
-		queue_free()
